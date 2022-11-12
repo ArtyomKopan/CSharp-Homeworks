3ceb83322377a36d73055ec675313c1946d1d906
ArtyomKopan <artem.kopan.2003@mail.ru>
2022-11-12 18:43:52 +0300

    I added more tests

--- ControlWorkTest/UnitTest1.cs
+++ ControlWorkTest/UnitTest1.cs
@@ -73,4 +73,44 @@ public class Tests
 
         Assert.That(s1, Is.EqualTo("a"));
     }
+
+
+    [Test]
+    public async Task MultiThreadTest2()
+    {
+        var q = new ControlWork.ThreadSafetyQueue<string>();
+
+        var thread1 = new Task<string>(() =>
+        {
+            var result = q.Dequeue();
+
+            q.Enqueue("a", 10);
+            q.Enqueue("b", 2);
+            q.Enqueue("c", 2);
+
+            return result;
+        });
+
+        var thread2 = new Task<string>(() =>
+        {
+            q.Enqueue("d", 1);
+            q.Enqueue("e", 3);
+            q.Dequeue();
+            q.Dequeue();
+            var result = q.Dequeue();
+
+            return result;
+        });
+
+        thread1.Start();
+        thread2.Start();
+
+        var (result1, result2) = (await thread1, await thread2);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(result1, Is.EqualTo("d"));
+            Assert.That(result2, Is.EqualTo("b"));
+        });
+    }
 }
